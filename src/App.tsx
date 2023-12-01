642d53b674c81b725e537d893b38487e4fc3e332
petritsyn <andrey2343@yandex.ru>
2023-12-02 02:41:55 +0300

    sprint 01 week2 start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
-import {TaskType, Todolist} from "./Todolist/Todolist";
+import {Todolist} from './Todolist';
 
-export const App = () => {
+function App() {
 
-    const tasks_1: Array<TaskType> = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: false},
-        {id: 3, title: 'React', isDone: true},
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
     ]
-    const tasks_2: Array<TaskType> = [
-        {id: 4, title: 'Bear', isDone: true},
-        {id: 5, title: 'Fish', isDone: true},
-        {id: 6, title: 'Cheeps', isDone: false},
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
     ]
 
     return (
         <div className="App">
-            <Todolist title={'React'} tasks={tasks_1}/>
-            <Todolist title={'Redux'} tasks={tasks_2}/>
+            <Todolist title="What to learn" tasks={tasks1} />
+            <Todolist title="Songs" tasks={tasks2} />
         </div>
     );
 }
+
+export default App;
